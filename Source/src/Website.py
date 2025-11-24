a1bfc6d531906a0d9d2c473f583905eeb52f01e3
ppvyctor <pedro.vyctor00@gmail.com>
2025-11-24 20:28:36 -0300

    Adding the sistem of zip download button

--- Source/src/Website.py
+++ Source/src/Website.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import os
 import zipfile
+from io import BytesIO
 
 
 st.set_page_config(page_title="Business Game", page_icon="🎮")
@@ -25,6 +26,21 @@ st.markdown("<br><br><br><br>", unsafe_allow_html=True)
 
 #left, right = st.columns(2)
 
+def create_zip(source_folder):
+    """Cria um arquivo zip em memória a partir de uma pasta"""
+    zip_buffer = BytesIO()
+    
+    with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, dirs, files in os.walk(source_folder):
+            for file in files:
+                file_path = os.path.join(root, file)
+                arcname = os.path.relpath(file_path, source_folder)
+                zipf.write(file_path, arcname)
+    
+    zip_buffer.seek(0)
+    return zip_buffer.getvalue()
+
+
 # 1. Descobre o diretório onde este arquivo .py está localizado
 diretorio_script = os.path.dirname(os.path.abspath(__file__))
 
@@ -34,9 +50,15 @@ caminho_arquivo = os.path.join(diretorio_script, "Business_Game.rar")
 #with left:
 st.markdown('<h1>Windows</h1>', unsafe_allow_html=True)
 
-with open("downloads\\Business_Game.rar", "r") as f:
-    st.download_button("Business Game.rar", f.read(), file_name="BusinessGame.rar", mime="application/x-rar-compressed", on_click = create_zip)
+# Botão que gera o zip dinamicamente
+if st.button("Gerar e Baixar ZIP"):
+    with st.spinner("Gerando arquivo ZIP..."):
+        zip_data = create_zip('Game')
+        st.download_button(
+            label="📥 Baixar Business Game.zip",
+            data=zip_data,
+            file_name="BusinessGame.zip",
+            mime="application/zip"
+        )
 
-with open("downloads\\Business_Game.zip", "rb") as f:
-    st.download_button("Business Game.zip", f.read(), file_name="BusinessGame.zip", mime="application/x-zip-compressed")
         
